9f3f3ba2fb953f629e77df886239b5cbd5a86613
Kayvan <kayvan.najafzadeh@gmail.com>
2015-04-14 10:37:39 -0700

    fix

--- app/src/main/java/com/doximity/androidpractice/MainActivity.java
+++ app/src/main/java/com/doximity/androidpractice/MainActivity.java
@@ -6,9 +6,14 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+
+import butterknife.ButterKnife;
 import butterknife.InjectView;
 import retrofit.Callback;
 import retrofit.RestAdapter;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
 import retrofit.http.GET;
 import retrofit.http.Query;
 
@@ -17,40 +22,42 @@ import retrofit.http.Query;
  */
 public class MainActivity extends Activity {
     @InjectView(R.id.list)
-    ListView LISTVIEW;
+    ListView mListView;
     @InjectView(R.id.query)
-    EditText QUERY;
+    EditText mQuery;
     @InjectView(R.id.search)
-    Button SEARCH;
+    Button mSearch;
 
     /* TODO Make a request with the input query and get the articles
     Display the headline of articles in a listView
     Tapping each item in listView should open that article URL in a browser
     */
     interface api{
-        @GET("/svc/search/v2/articlesearch.json?q={query}&page=2&sort=oldest&api-key=f8e3e0aab0c3cc0ff9c9daac65bb4863:0:71846732")
-        public void getArticles(@Query("query") String q, Callback<NytData> res);
+        @GET("/svc/search/v2/articlesearch.json?page=2&sort=oldest&api-key=f8e3e0aab0c3cc0ff9c9daac65bb4863:0:71846732")
+        public void getArticles(@Query("q") String q, Callback<NytData> res);
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+        ButterKnife.inject(this);
 
         RestAdapter restAdapter = new RestAdapter.Builder().setEndpoint("http://api.nytimes.com").build();
-//        restAdapter.create(api.class).getArticles(YOUR_QUERY_TEXT,new Callback<NytData>() {
-//            @Override
-//            public void success(NytData nytData, retrofit.client.Response response2) {
-//                ArrayList<Docs> docs = nytData.response.docs;
-//                // TODO fill your list
-//
-//            }
-//
-//            @Override
-//            public void failure(RetrofitError error) {
-//                // Do nothing
-//            }
-//        });
+
+        restAdapter.create(api.class).getArticles(mQuery.getText().toString(),new Callback<NytData>() {
+        @Override
+        public void success(NytData nytData, Response response) {
+            ArrayList<Docs> docs = nytData.response.docs;
+            // TODO fill your list
+        }
+
+        @Override
+        public void failure(RetrofitError error) {
+
+        }
+        });
+
     }
 
 
